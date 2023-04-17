31ae53ec91edb2e126b137f5549a3e29df9022de
HughMorris01 <gfarrell2@Live.com>
2023-04-17 18:21:29 -0400

    Add Headings

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import SlickCarousel from './SlickCarousel';
 
 const App = () => {
   return (
-    <main>
+    <main style={{ textAlign: 'center' }}>
+      <h2>Custom Built Carousel</h2>
       <Carousel />
+      <h2>React Slick Carousel</h2>
       <SlickCarousel />
     </main>
   );
